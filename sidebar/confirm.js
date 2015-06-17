870db4835e2b1190e151f1882eaf11143da09d77
Emily Zhang <e.zhang@berkeley.edu>
2015-06-16 23:23:01 -0700

    transitions

--- sidebar/confirm.js
+++ sidebar/confirm.js
@@ -1,10 +1,25 @@
 document
     .getElementById("confirmClickActionElementId")
     .addEventListener("click", function( e ){ //e => event
-        if( ! confirm("Do you really want to do this?") ){
+        if( ! confirm("do you really want to do this?") ){
             e.preventDefault(); // ! => don't want to do this
         } else {
             //want to do this! => maybe do something about it?
-            alert('Ok, lets do this!');
+            confirm('seriously though this is a mistake');
+            confirm('i\'m warning you');
+            confirm('i urge you to reconsider');
+            confirm('or at least read the about page so you know what\'s going on');
+            confirm('you are one persistent person wow');
+            confirm('ok you should be prepared');
+            confirm('!');
+            confirm('u!');
+            confirm('u');
+            confirm('uuuuu!');
+            confirm('u!');
+            confirm('u');
+            confirm('u');
+            confirm('wow you\'re still here');
+            confirm('guess i\'ve got no choice');
+            confirm('ok, lets do this!');
         }
     });
